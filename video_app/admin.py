8fb7925695cae84fd639b5db58fb3d2379a868df
Annette Lasar <a_lasar@web.de>
2025-08-06 17:17:21 +0200

    Create django admin for videos and user watch progress

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Video, VideoStreamVariant, UserWatchProgress
 
-# Register your models here.
+
+class VideoStreamVariantInline(admin.TabularInline):
+    model = VideoStreamVariant
+    extra = 1
+    exclude = ('manifest_path',)
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "available_resolutions", "created_at")
+    search_fields = ("title", "category")
+    list_filter = ("category", "created_at")
+    ordering = ("-created_at",)
+    inlines = [VideoStreamVariantInline]
+
+    def available_resolutions(self, obj):
+        return ", ".join(
+            variant.resolution for variant in obj.variants.all()
+        )
+    available_resolutions.short_description = "Resolutions"
+
+
+@admin.register(UserWatchProgress)
+class UserWatchProgressAdmin(admin.ModelAdmin):
+    list_display = ("user", "video", "resolution",
+                    "last_position_seconds", "updated_at")
+    list_filter = ("resolution", "updated_at")
+    search_fields = ("user__username", "video__title")
+    ordering = ("-updated_at",)
